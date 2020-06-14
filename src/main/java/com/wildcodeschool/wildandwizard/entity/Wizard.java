8c771cd12cde78875b4f0ef00fa2f44ee36b3b5e
lePitch <pierre.henri.jautard@gmail.com>
2020-06-14 14:09:47 +0200

    fix wizard

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import java.sql.Date;
 
 @Entity
@@ -19,6 +22,10 @@ public class Wizard {
     private String biography;
     private boolean muggle;
 
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    private School school;
+    
     public Wizard() {
     }
 
@@ -77,4 +84,12 @@ public class Wizard {
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
     }
-}
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+}   
